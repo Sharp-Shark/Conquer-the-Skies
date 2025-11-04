43c44d8a6807dd6f2fa9bcd15bbf803e9e08af2e
Sharp-Shark <arthurbmc6@gmail.com>
2025-11-04 20:43:37 -0300

    1.0.16
    Undid 1.0.15

--- Conquer the Skies/CSharp/Shared/noWater.cs
+++ Conquer the Skies/CSharp/Shared/noWater.cs
@@ -234,7 +234,8 @@ namespace NoWater
                 __instance.inWater = false;
                 __instance.headInWater = false;
                 __instance.RefreshFloorY(deltaTime, false);
-                if (__instance.TargetMovement.X == 0f && __instance.Collider.LinearVelocity.Y < -1.0f)
+                // __instance.TargetMovement.X == 0f
+                if (__instance.Collider.LinearVelocity.Y < -1.0f)
                 {
                     __instance.ForceRefreshFloorY();
                 }
