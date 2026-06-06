3d395a289f8c3f3b0212c794084461422e255aad
Sharp-Shark <arthurbmc6@gmail.com>
2026-06-06 18:41:58 -0300

    1.0.32
    Added a guard clause cuz a guy asked. Will probably not fix whatever his issue was.

--- Conquer the Skies/CSharp/Shared/noWater.cs
+++ Conquer the Skies/CSharp/Shared/noWater.cs
@@ -880,6 +880,7 @@ namespace NoWater
         }
         public static void OverridePropulsionUse(Barotrauma.Items.Components.Propulsion __instance, float deltaTime, Character character)
         {
+            if (character == null || character.Removed) { return false; }
             if (!__instance.IsActive) { return; }
 
             Vector2 dir = character.CursorPosition - character.Position;
